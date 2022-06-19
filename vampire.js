275df920169a7da5135968ea5a95c060019afa9c
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-19 18:45:47 -0300

    working functions

--- vampire.js
+++ vampire.js
@@ -10,22 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let degreeOfSeparation = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      degreeOfSeparation++;
+    }
+    return degreeOfSeparation;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    const vampire1 = this.numberOfOffspring;
+    const vampire2 = vampire.numberOfOffspring;
 
+    if (vampire1 > vampire2) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
